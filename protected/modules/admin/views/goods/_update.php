cb6596e4901adcffba85ebdce255bc8955a51f2d
Alexander <scorgi86@gmail.com>
2013-12-17 11:13:06 +0600

    pravki qeqweqwe

--- protected/modules/admin/views/goods/_update.php
+++ protected/modules/admin/views/goods/_update.php
@@ -16,14 +16,14 @@
 	<?php echo $form->dropDownListControlGroup($model, 'status', Goods::getStatusAliases(), array('class'=>'span8', 'displaySize'=>1)); ?>
 	<div class="attrs">
 		<table>
-			
+			<tr>
 				<?
 					foreach ($attrs as $key => $value) {
-						$att=CategoryAttrs::model()->findByPk($value->id);
-						print('<tr><td width="150">'.$att->name.'</td><td><input type="text" name="attrs['.$value->id.']" value="'.$value->attr_value.'"></td></tr>');
+						$att=CategoryAttrs::model()->findBypk($id);
+						print('<td>'.$att->name.'<input type="text" name="attrs['.$value->id.']" value="'.$value->attr_value.'"></td>');
 					}
 				?>
-			
+			</tr>
 		</table>
 	</div>
 	<div class='control-group'>
